8389bc0c8b71a29d609068785f515abb0fe7bd95
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-08 09:36:24 +0500

    New Commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,25 @@ import '@gravity-ui/uikit/styles/styles.css';
 import './styles/globals.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query"
+
+const queryClient = new QueryClient({
+      defaultOptions:{
+         queries:{
+            retry:1,
+            staleTime:1000 * 60 * 5,
+            // @ts-ignore
+            cacheTime:1000 * 60 * 10,
+            refetchOnWindowFocus:false
+         }
+      }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <BrowserRouter>
-         <App />
+       <QueryClientProvider client={queryClient}>
+         <App/>
+       </QueryClientProvider>
     </BrowserRouter>
 );
---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+import Home from "./Home"
+import AddCourses from "./AddCourses"
+
+export {
+    Home,
+    AddCourses
+}
